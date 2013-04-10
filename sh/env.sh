343a9e2d9d11acdf10ae994f22fb7d67ad9a729b
Chris Ledet <christopher.ledet@gmail.com>
2013-04-10 15:27:52 -0400

    Updated shell env variables.

--- sh/env.sh
+++ sh/env.sh
@@ -1,4 +1,8 @@
-export EDITOR="vim"
-export DYLD_LIBRARY_PATH="/usr/local/Cellar/mysql/5.5.14/lib:/usr/local/mysql/lib:$HOME/.oracle/instantclient"
+export EDITOR="subl -w"
 export CC=`which clang`
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/bin:$PATH:$HOME/bin"
+
+# rbenv
+eval "$(rbenv init -)"
+export RUBYLIB=/usr/local/lib/ruby/1.8:/usr/local/lib/ruby/1.8/universal-darwin12.0
